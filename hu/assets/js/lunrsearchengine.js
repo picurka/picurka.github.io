f56bbc63bb9bf08213baf3d2d9dcc0ccc23de070
picurka <77541447+picurka@users.noreply.github.com>
2021-01-31 23:59:18 +0100

    add cookie

--- hu/assets/js/lunrsearchengine.js
+++ hu/assets/js/lunrsearchengine.js
@@ -21,11 +21,16 @@ var documents = [{
     "body": "{% if page. url == “/” %}       Featured:       {% for post in site. posts %}    {% if post. featured == true %}      {% include featuredbox. html %}    {% endif %}  {% endfor %}  {% endif %}       All Stories:         {% for post in paginator. posts %}    {% include postbox. html %}    {% endfor %}    {% include pagination. html %}"
     }, {
     "id": 4,
+    "url": "/hu/terms",
+    "title": "Terms and Privacy Policy",
+    "body": ""
+    }, {
+    "id": 5,
     "url": "/hu/robots.txt",
     "title": "",
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
-    "id": 5,
+    "id": 6,
     "url": "/hu/welcome-to-jekyll/",
     "title": "Szia Jekyll!",
     "body": "2021/01/25 - You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when a file is updated. Jekyll requires blog post files to be named according to the following format: YEAR-MONTH-DAY-title. MARKUP Where YEAR is a four-digit number, MONTH and DAY are both two-digit numbers, and MARKUP is the file extension representing the format used in the file. After that, include the necessary front matter. Take a look at the source for this post to get an idea about how it works. Jekyll also offers powerful support for code snippets: def print_hi(name) puts  Hi, #{name} endprint_hi('Tom')#=&gt; prints 'Hi, Tom' to STDOUT. Check out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can ask them on Jekyll Talk. "
